efebfae8649591c8183992fe106566fe52b8576d
vineetkumarverma <vineet88.kiet@gmail.com>
2017-04-16 15:35:01 +0530

    intital commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-    <apis></apis>
+    <h1 class="title">API Registry</h1>
+    <nav>
+      <a routerLink="/apis-group" routerLinkActive="active">API Groups</a>
+    
+      <a routerLink="/admin" routerLinkActive="active">Admin</a>
+      <a routerLink="/login" routerLinkActive="active">Login</a>
+      <a [routerLink]="[{ outlets: { popup: ['compose'] } }]">Contact</a>
+    </nav>
+    <router-outlet></router-outlet>
+    <router-outlet name="popup"></router-outlet>
   `
 })
-export class AppComponent  { 
+export class AppComponent {
 }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,46 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 import {HttpModule} from '@angular/http';
-import { AppComponent }  from './app.component';
-import { ApisComponent } from './components/apisgroup.component';
-import { ApiDetailsComponent } from './components/apiDetails.component';
+// import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
-import {routes} from './app.router';
+import { Router } from '@angular/router';
+
+import { AppComponent }            from './app.component';
+import { AppRoutingModule }        from './app-routing.module';
+
+
+import { ComposeMessageComponent } from './compose-message.component';
+import { LoginRoutingModule }      from './login-routing.module';
+import { LoginComponent }          from './login.component';
+import { PageNotFoundComponent }   from './not-found.component';
+
+import { DialogService }           from './dialog.service';
+
+
 @NgModule({
-  imports:      [ BrowserModule,FormsModule,HttpModule,Angular2FontawesomeModule,routes ],
-  declarations: [ AppComponent,ApisComponent,ApiDetailsComponent ],
-  bootstrap:    [ AppComponent ],
-  
+  imports: [
+    BrowserModule,
+    FormsModule,
+    LoginRoutingModule,
+    AppRoutingModule,
+    HttpModule
+    //,BrowserAnimationsModule
+  ],
+  declarations: [
+    AppComponent,
+    ComposeMessageComponent,
+    LoginComponent,
+    PageNotFoundComponent
+  ],
+  providers: [
+    DialogService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+  // Diagnostic only: inspect router configuration
+  constructor(router: Router) {
+    console.log('Routes: ', JSON.stringify(router.config, undefined, 2));
+  }
+}
